1f1c5cff262afa39529973eb55c2c551cc799a75
Josh <Rdeckj@gmail.com>
2019-01-13 20:34:06 -0500

    Add abbreviations vector

--- src/table_name_minifier/core.clj
+++ src/table_name_minifier/core.clj
@@ -1,8 +1,22 @@
 (ns table-name-minifier.core
   (:require [clojure.string :as str]))
 
-(def commands ["--help"]
+(defn command-help
+  "Display the help screen"
+  []
+  (println "Usage: tnmin [--help] <command> [<args>]")
   )
+
+(def commands [{:name "--help" :function command-help}])
+(def abbreviations [{:word "percent" :abbreviation "pct"}
+                    {:word "street" :abbreviation "st"}
+                    {:word "avenue" :abbreviation "ave"}])
+
+(defn get-command-fn
+  "Search command list for valid command flags"
+  [optional-args]
+  (when-not (empty? optional-args)
+    (:function (first (filter #(= optional-args (:name %)) commands)))))
 
 (defn command-not-found
   "Print command not found error to user"
@@ -13,30 +27,40 @@
   (flush)
   )
 
-(defn find-command
-  "Search command list for valid command flags"
-  [optional-args]
-  (if (empty? optional-args)
-    ()
-    (some #(= optional-args %) commands))
-  )
-
 (defn remove-vowels
   "Remove vowels from the string"
   [input]
   (str/replace input #"[aeiou]" ""))
 
+(defn process-special-words
+  ""
+  [word]
+  (let [result (:abbreviation (first (filter #(= word (:word %)) abbreviations)))]
+    (if-not (empty? result)
+      (str result)
+      (str word))))
+
 (defn minify-input
   "Condense the input string"
   [long-name]
-  (remove-vowels long-name))
+  (remove-vowels (process-special-words long-name)))
+
+(defn handle-commands
+  "Handle special commands"
+  [possible-command]
+  (let [command-fn (get-command-fn possible-command)]
+    (if command-fn
+      (command-fn)
+      ())))
 
 (defn -main
   "Take user input and process"
   [& args]
-  (let [[first & optional-args] args]
-    (if (find-command optional-args)
-      ()
-      (command-not-found optional-args))
-    (println (minify-input first))
-))
+  (let [[first-arg & remaining-args] args]
+    (if (handle-commands first-arg)
+      (do
+        (let [result (minify-input first-arg)]
+          (println result)
+          result))
+      ())
+    ))
--- test/table_name_minifier/core_test.clj
+++ test/table_name_minifier/core_test.clj
@@ -2,6 +2,22 @@
   (:require [clojure.test :refer :all]
             [table-name-minifier.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest basic-input
+  (testing "basic input minifies by removing vowels"
+    (is (= (-main "testing") "tstng"))))
+
+(deftest help-command
+  (testing "help command returns nothing"
+    (is (= (-main "--help") ()))))
+
+(deftest help-input
+  (testing "help word returns minified version"
+    (is (= (-main "help") "hlp"))))
+
+(deftest special-word-percent
+  (testing "percent becomes pct"
+    (is (= (-main "percent") "pct"))))
+
+;(deftest special-word-road
+;  (testing "avenue becomes ave"
+;    (is (= (-main "avenue") "ave"))))
