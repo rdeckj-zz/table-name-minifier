9f6c6f0bd096930095dd62cfa30962b05ab6339a
Josh <Rdeckj@gmail.com>
2019-06-18 21:15:09 -0400

    Test replace-abbr

--- test/table_name_minifier/core_test.clj
+++ test/table_name_minifier/core_test.clj
@@ -4,9 +4,22 @@
 
 (deftest -main-tests
   (testing "main prints correct value"
-    (is (= (with-out-str (-main "really_long_table_name_over_32_characters_and_it_sucks "))
-           "rlly_lng_tbl_nm_vr_32_chrctrs_nd_t_scks\n")))
+    (is (= (clojure.string/trim-newline (with-out-str (-main "really_long_table_name_over_32_characters_and_it_sucks ")))
+           "rlly_lng_tbl_nm_vr_32_chrctrs_nd_t_scks")))
 
   (testing "main prints correct value when passed command"
-    (is (= (with-out-str (-main "--verbose really_long_table_name_over_32_characters_and_it_sucks "))
-           "rlly_lng_tbl_nm_vr_32_chrctrs_nd_t_scks\n"))))
+    (is (= (clojure.string/trim-newline (with-out-str (-main "--verbose really_long_table_name_over_32_characters_and_it_sucks ")))
+           "rlly_lng_tbl_nm_vr_32_chrctrs_nd_t_scks"))))
+
+(deftest replace-abbr-tests
+  (testing "single word"
+    (is (= (replace-abbr "pound") "lb")))
+
+  (testing "abbreviation word at end"
+    (is (= (replace-abbr "some_percent") "some_pct")))
+
+  (testing "abbreviation word at start"
+    (is (= (replace-abbr "state_test") "st_test")))
+
+  (testing "abbreviation word in middle"
+    (is (= (replace-abbr "something_state_test") "something_st_test"))))
